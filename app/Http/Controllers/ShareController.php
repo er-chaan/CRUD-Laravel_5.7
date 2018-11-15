55519d3c04f3b857d816c0b37c8a76ea213f4044
root <root@localhost.localdomain>
2018-11-15 11:50:29 +0530

    create operations

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -23,7 +23,7 @@ class ShareController extends Controller
      */
     public function create()
     {
-        //
+        return view('shares.create');
     }
 
     /**
@@ -34,7 +34,18 @@ class ShareController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'share_name'=>'required',
+            'share_price'=> 'required|integer',
+            'share_qty' => 'required|integer'
+          ]);
+          $share = new Share([
+            'share_name' => $request->get('share_name'),
+            'share_price'=> $request->get('share_price'),
+            'share_qty'=> $request->get('share_qty')
+          ]);
+          $share->save();
+          return redirect('/shares')->with('success', 'Stock has been added');
     }
 
     /**
