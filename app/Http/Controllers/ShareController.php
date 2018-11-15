ccf6106b077a0c03b5c42b35699409e0369074bc
root <root@localhost.localdomain>
2018-11-15 12:02:36 +0530

    retrieve operations

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Share;
 
 class ShareController extends Controller
 {
@@ -13,7 +14,8 @@ class ShareController extends Controller
      */
     public function index()
     {
-        //
+        $shares = Share::all();
+        return view('shares.index', compact('shares'));
     }
 
     /**
