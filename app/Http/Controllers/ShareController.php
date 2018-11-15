c5f0d8b7baf229eb7a13138ee5e89e19c4d9c861
root <root@localhost.localdomain>
2018-11-15 12:07:22 +0530

    update operation

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -69,7 +69,8 @@ class ShareController extends Controller
      */
     public function edit($id)
     {
-        //
+        $share = Share::find($id);
+        return view('shares.edit', compact('share'));
     }
 
     /**
@@ -81,7 +82,17 @@ class ShareController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate([
+            'share_name'=>'required',
+            'share_price'=> 'required|integer',
+            'share_qty' => 'required|integer'
+          ]);
+          $share = Share::find($id);
+          $share->share_name = $request->get('share_name');
+          $share->share_price = $request->get('share_price');
+          $share->share_qty = $request->get('share_qty');
+          $share->save();
+          return redirect('/shares')->with('success', 'Stock has been updated');
     }
 
     /**
